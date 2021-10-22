ed4cfc9d2ffc5fc69ba000cdae25b87c62adcaae
tobiasorosa <tobias.rosa2000@gmail.com>
2021-10-22 14:13:08 -0300

    Repos list styled

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repos } from './styles';
 import logo from '../../assets/logo.svg';
 
 export const Dashboard: React.FC = () => {
@@ -13,6 +14,20 @@ export const Dashboard: React.FC = () => {
 				<input placeholder='username/repository_name' />
 				<button type='submit'>Buscar</button>
 			</Form>
+
+			<Repos>
+				<a href='/repositories'>
+					<img
+						src='https://avatars.githubusercontent.com/u/68042466?v=4'
+						alt='Repositório'
+					/>
+					<div>
+						<strong>tobiasorosa/pokedex</strong>
+						<p>Repositorio da pokedex</p>
+					</div>
+					<FiChevronRight size={20} />
+				</a>
+			</Repos>
 		</>
 	);
 };
