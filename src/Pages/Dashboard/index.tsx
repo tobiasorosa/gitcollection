02582a11b6c137d05a8c8e6e4e150da3b1935ec7
tobiasorosa <tobias.rosa2000@gmail.com>
2021-10-25 13:02:00 -0300

    Refactor array map

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,32 +1,99 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
 
-import { Title, Form, Repos } from './styles';
+import { api } from '../../services/api';
+import { Title, Form, Repos, Error } from './styles';
 import logo from '../../assets/logo.svg';
 
+interface GithubRepository {
+	full_name: string;
+	description: string;
+	owner: {
+		login: string;
+		avatar_url: string;
+	};
+}
+
 export const Dashboard: React.FC = () => {
+	const [repos, setRepos] = React.useState<GithubRepository[]>(() => {
+		const storageRepos = localStorage.getItem('@GitCollection:repositories');
+
+		if (storageRepos) {
+			return JSON.parse(storageRepos);
+		}
+
+		return [];
+	});
+	const [newRepo, setNewRepo] = React.useState('');
+	const [inputError, setInputError] = React.useState('');
+	const formEl = React.useRef<HTMLFormElement | null>(null);
+
+	React.useEffect(() => {
+		localStorage.setItem('@GitCollection:repositories', JSON.stringify(repos));
+	}, [repos]);
+
+	function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
+		setNewRepo(event.target.value);
+	}
+
+	async function handleAddRepo(
+		event: React.FormEvent<HTMLFormElement>
+	): Promise<void> {
+		event.preventDefault();
+
+		if (!newRepo) {
+			setInputError('Informe o username/repositório');
+			return;
+		}
+		try {
+			const response = await api.get<GithubRepository>(`repos/${newRepo}`);
+			const repository = response.data;
+			setRepos([...repos, repository]);
+			formEl.current?.reset();
+			setNewRepo('');
+			setInputError('');
+		} catch (err) {
+			setInputError('Repositório não encontrado');
+		}
+	}
+
 	return (
 		<>
 			<img src={logo} alt='GitCollection' />
 			<Title>Catálogo de repositórios do GitHub</Title>
 
-			<Form>
-				<input placeholder='username/repository_name' />
+			<Form
+				ref={formEl}
+				hasError={Boolean(inputError)}
+				onSubmit={handleAddRepo}
+			>
+				<input
+					placeholder='username/repository_name'
+					onChange={handleInputChange}
+				/>
 				<button type='submit'>Buscar</button>
 			</Form>
 
+			{inputError && <Error>{inputError}</Error>}
+
 			<Repos>
-				<a href='/repositories'>
-					<img
-						src='https://avatars.githubusercontent.com/u/68042466?v=4'
-						alt='Repositório'
-					/>
-					<div>
-						<strong>tobiasorosa/pokedex</strong>
-						<p>Repositorio da pokedex</p>
-					</div>
-					<FiChevronRight size={20} />
-				</a>
+				{repos.map((repository, index) => (
+					<Link
+						to={`/repositories/${repository.full_name}`}
+						key={repository.full_name + index}
+					>
+						<img
+							src={repository.owner.avatar_url}
+							alt={repository.owner.login}
+						/>
+						<div>
+							<strong>{repository.full_name}</strong>
+							<p>{repository.description}</p>
+						</div>
+						<FiChevronRight size={20} />
+					</Link>
+				))}
 			</Repos>
 		</>
 	);
