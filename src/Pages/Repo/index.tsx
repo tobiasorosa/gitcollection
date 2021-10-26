670b2a1fd50ffb3e8f5d22c25e7fe7664bbce765
tobiasorosa <tobias.rosa2000@gmail.com>
2021-10-26 09:26:28 -0300

    webpack performance

--- src/Pages/Repo/index.tsx
+++ src/Pages/Repo/index.tsx
@@ -32,7 +32,7 @@ interface GithubIssue {
 	};
 }
 
-export const Repo: React.FC = () => {
+const Repo: React.FC = () => {
 	const [repository, setRepository] = React.useState<GithubRepository | null>(
 		null
 	);
@@ -102,3 +102,5 @@ export const Repo: React.FC = () => {
 		</>
 	);
 };
+
+export default Repo;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Dashboard } from '../Pages/Dashboard';
-import { Repo } from '../Pages/Repo';
+// import { Dashboard } from '../Pages/Dashboard';
+// import { Repo } from '../Pages/Repo';
+
+const Dashboard = React.lazy(() =>
+	import(
+		/*webpackPrefetch: true */
+		/* webpackChunkName: "dashboard" */ '../Pages/Dashboard'
+	)
+);
+const Repo = React.lazy(() =>
+	import(
+		/*webpackPrefetch: true */
+		/* webpackChunkName: "repo" */ '../Pages/Repo'
+	)
+);
 
 export const Routes: React.FC = () => {
 	return (
-		<Switch>
-			<Route component={Dashboard} path='/' exact />
-			<Route component={Repo} path='/repositories/:repository+' />
-		</Switch>
+		<React.Suspense fallback={'Loading...'}>
+			<Switch>
+				<Route component={Dashboard} path='/' exact />
+				<Route component={Repo} path='/repositories/:repository+' />
+			</Switch>
+		</React.Suspense>
 	);
 };
